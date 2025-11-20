3a141fedc097ffc9976bc52bbd85c9ae892f334e
Zyphon <elgator833@gmail.com>
2025-11-20 17:07:25 -0300

    header estilizado

--- assets/java/mostrarImagem.js
+++ assets/java/mostrarImagem.js
@@ -13,6 +13,9 @@ const engasgotittle = document.querySelector("#banner-tittle-text-3");
 option1.style.background = "white";
 option2.style.background = "white";
 option3.style.background = "white";
+coracao.style.display = "flex";
+coracaotittle.style.display = "flex";
+option1.style.background = "black";
 function selecionarAnterior() {
     option = option - 1;
     if (option == 0) {
