0e0a7a3354ac830e4bab26db6a0c59c0670f6dd4
Zyphon <elgator833@gmail.com>
2025-11-20 16:07:52 -0300

    banner

--- assets/java/mostrarImagem.js
+++ assets/java/mostrarImagem.js
@@ -1,4 +1,4 @@
-var option = 1;
+let option = 1;
 const coracao = document.querySelector("#carrossel-1");
 const veneno = document.querySelector("#carrossel-2");
 const engasgo = document.querySelector("#carrossel-3");
@@ -10,38 +10,86 @@ const front = document.querySelector("#arrow-icon-right");
 const coracaotittle = document.querySelector("#banner-tittle-text-1");
 const venenotittle = document.querySelector("#banner-tittle-text-2");
 const engasgotittle = document.querySelector("#banner-tittle-text-3");
-option1.style.background="white";
-option2.style.background="white";
-option3.style.background="white";
-function selecionarAnterior(){
-    option=option-1;
-    if(option=0){
-        option=3;
+option1.style.background = "white";
+option2.style.background = "white";
+option3.style.background = "white";
+function selecionarAnterior() {
+    option = option - 1;
+    if (option == 0) {
+        option = 3;
     }
-    console.log("voceclicou");
+    if (option == 1) {
+        coracao.style.display = "flex";
+        coracaotittle.style.display = "flex";
+        option1.style.background = "black";
+        veneno.style.display = "none";
+        venenotittle.style.display = "none";
+        option2.style.background = "white";
+        engasgo.style.display = "none";
+        engasgotittle.style.display = "none";
+        option3.style.background = "white";
+    }
+    else if (option == 2) {
+        coracao.style.display = "none";
+        coracaotittle.style.display = "none";
+        option1.style.background = "white";
+        veneno.style.display = "flex";
+        venenotittle.style.display = "flex";
+        option2.style.background = "black";
+        engasgo.style.display = "none";
+        engasgotittle.style.display = "none";
+        option3.style.background = "white";
+    }
+    else if (option == 3) {
+        coracao.style.display = "none";
+        coracaotittle.style.display = "none";
+        option1.style.background = "white";
+        veneno.style.display = "none";
+        venenotittle.style.display = "none";
+        option2.style.background = "white";
+        engasgo.style.display = "flex";
+        engasgotittle.style.display = "flex";
+        option3.style.background = "black";
+    }
 }
-back.addEventListener("click",selecionarAnterior);
-if(option==1){
-    coracao.style.display="flex";
-    coracaotittle.style.display="flex";
-    option1.style.background="black";
+function selecionarProximo() {
+    option = option + 1;
+    if (option > 3) {
+        option = 1;
+    }
+    if (option == 1) {
+        coracao.style.display = "flex";
+        coracaotittle.style.display = "flex";
+        option1.style.background = "black";
+        veneno.style.display = "none";
+        venenotittle.style.display = "none";
+        option2.style.background = "white";
+        engasgo.style.display = "none";
+        engasgotittle.style.display = "none";
+        option3.style.background = "white";
+    }
+    else if (option == 2) {
+        coracao.style.display = "none";
+        coracaotittle.style.display = "none";
+        option1.style.background = "white";
+        veneno.style.display = "flex";
+        venenotittle.style.display = "flex";
+        option2.style.background = "black";
+        engasgo.style.display = "none";
+        engasgotittle.style.display = "none";
+        option3.style.background = "white";
+    }
+    else if (option == 3) {
+        coracao.style.display = "none";
+        coracaotittle.style.display = "none";
+        option1.style.background = "white";
+        veneno.style.display = "none";
+        venenotittle.style.display = "none";
+        option2.style.background = "white";
+        engasgo.style.display = "flex";
+        engasgotittle.style.display = "flex";
+        option3.style.background = "black";
+    }
 }
-else if(option==2){
-    coracao.style.display="none";
-    coracaotittle.style.display="none";
-    option1.style.background="white";
-    veneno.style.display="flex";
-    venenotittle.style.display="flex";
-    option2.style.background="black";
-}
-else if(option==3){
-    coracao.style.display="none";
-    coracaotittle.style.display="none";
-    option1.style.background="white";
-    veneno.style.display="none";
-    venenotittle.style.display="none";
-    option2.style.background="white";
-    engasgo.style.display="flex";
-    engasgotittle.style.display="flex";
-    option3.style.background="black";
-}
+back.addEventListener("click", selecionarAnterior);
+front.addEventListener("click", selecionarProximo);
